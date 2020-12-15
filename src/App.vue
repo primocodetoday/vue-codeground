52963028fa59c683e418cebc4b0668b244fafc94
primocodetoday <primocodetoday@gmail.com>
2020-12-15 20:49:30 +0100

    lib: vue update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -11,7 +10,6 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
-
   components: {
     HelloWorld
   }
@@ -21,15 +19,10 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-
   -webkit-font-smoothing: antialiased;
-
   -moz-osx-font-smoothing: grayscale;
-
   text-align: center;
-
   color: #2c3e50;
-
   margin-top: 60px;
 }
 </style>
